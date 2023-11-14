14be46589b224873783eb8c22ee620ee820ebb82
Pierrot LC <pstmrtem@outlook.com>
2023-11-14 19:27:34 +0100

    ✨ nvim: better lua_ls integration

--- nvim/keybindings.lua
+++ nvim/keybindings.lua
@@ -2,6 +2,7 @@
 -- ==                             KEYBINDINGS                              == --
 -- ========================================================================== --
 vim.keymap.set("n", "<leader>w", "<cmd>write<cr>", { desc = "Save" })
+vim.keymap.set("n", "<leader>q", "<cmd>quit<cr>", { desc = "Quit" })
 vim.keymap.set("n", "<leader>h", "<cmd>nohl<cr>", { desc = "Remove highlights" })
 vim.keymap.set({ "n", "x" }, "cp", '"+y', { desc = "Copy to system clipboard" })
 vim.keymap.set({ "n", "x" }, "cv", '"+p', { desc = "Paste from system clipboard" })
@@ -13,7 +14,3 @@ vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Quick move to bottom buffer" })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Quick move to upper buffer" })
 vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Quick move to left buffer" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Quick move to right buffer" })
-
--- Neovim configuration
-vim.keymap.set("n", "<leader>vr", "<cmd>source $MYVIMRC<cr>", { desc = "Reload the vim configuration" })
-vim.keymap.set("n", "<leader>vc", "<cmd>edit $MYVIMRC<cr>", { desc = "Edit the vim configuration" })
--- nvim/plugins/which-key/config.lua
+++ nvim/plugins/which-key/config.lua
@@ -2,6 +2,3 @@ vim.opt.timeout = true
 vim.opt.timeoutlen = 300
 
 require("which-key").setup()
-require("which-key").register({
-	["<Leader>v"] = { name = "+Neovim" },
-})
