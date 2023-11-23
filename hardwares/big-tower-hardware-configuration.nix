4a9f48c727bcf95a6346131ae6bcf99976d138c5
Pierrot LC <pstmrtem@outlook.com>
2023-11-23 18:31:28 +0100

    🐛 hardware: big-tower nvidia

--- hardwares/big-tower-hardware-configuration.nix
+++ hardwares/big-tower-hardware-configuration.nix
@@ -12,17 +12,18 @@
   networking.hostName = "big-tower";
 
   # Bootloader.
-  # boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub = {
-    enable = true;
-    device = "nodev";
-    useOSProber = true;
-  };
+  # boot.loader.grub = {
+  #   enable = true;
+  #   device = "nodev";
+  #   useOSProber = true;
+  # };
 
   # Allow unfree packages (NVIDIA).
   nixpkgs.config.allowUnfree = true;
 
+  # See https://nixos.wiki/wiki/Nvidia.
   hardware.opengl = {
     enable = true;
     driSupport = true;
@@ -37,8 +38,15 @@
     modesetting.enable = true;
 
     # Use the open source version of the kernel module.
-    # Only available on driver 515.43.04+
-    open = true;
+    # Only available on driver 515.43.04+ on recents GOUs.
+    # See https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+    open = false;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    powerManagement.enable = false;
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
 
     # Enable the nvidia settings menu.
     nvidiaSettings = true;
