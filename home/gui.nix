8949d597449c2b32658a07289621344214909038
Pierrot LC <pstmrtem@outlook.com>
2024-01-15 13:55:01 +0100

    ✨ add obs-studio with virtual webcam support

--- home/gui.nix
+++ home/gui.nix
@@ -1,6 +1,13 @@
 { config, pkgs, ... }:
 
 {
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+    ];
+  };
+
   home.packages = with pkgs; [
     keymapp
     mullvad-vpn
