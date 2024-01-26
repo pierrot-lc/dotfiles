ab7c4b0100a223645dcd41c3fe7281608f295f34
Pierrot LC <pierrotlc@proton.me>
2024-01-26 11:16:12 +0100

    ✨ mails: setting my mails up

--- home/mails/default.nix
+++ home/mails/default.nix
@@ -1,36 +1,83 @@
-{ pkgs, ... }:
-
+{ lib, ... }:
 let
   private = import ./private.nix;
 in
 {
-  programs.mbsync.enable = true;
-  programs.msmtp.enable = true;
-  programs.notmuch = {
+  programs.thunderbird = {
     enable = true;
-    hooks = {
-      preNew = "mbsync --all";
+    profiles.pierre = {
+      isDefault = true;
     };
   };
-  programs.aerc.enable = true;
-  programs.aerc.extraConfig.general.unsafe-accounts-conf = true;
 
   accounts.email.accounts = {
-    perso-outlook = {
-      address = private.outlook_1;
+    "${private.outlook-1}" = {
+      address = private.outlook-1;
       primary = true;
+      realName = "Pierrot LC";
 
-      mbsync = {
+      thunderbird = {
         enable = true;
-        create = "maildir";
+        profiles = [ "pierre" ];
       };
-      msmtp.enable = true;
-      notmuch.enable = true;
-      aerc.enable = true;
 
       flavor = "outlook.office365.com";
-      passwordCommand = private.pass_outlook_1;
-      realName = "Pierrot LC";
+      smtp.host = lib.mkForce "smtp-mail.outlook.com";
+      passwordCommand = private.pass-outlook-1;
+    };
+
+    "${private.outlook-2}" = {
+      address = private.outlook-2;
+      realName = "Pierre Pereira";
+
+      thunderbird = {
+        enable = true;
+        profiles = [ "pierre" ];
+      };
+
+      flavor = "outlook.office365.com";
+      smtp.host = lib.mkForce "smtp-mail.outlook.com";
+      passwordCommand = private.pass-outlook-2;
+    };
+
+    "${private.gmail}" = {
+      address = private.gmail;
+      realName = "Pierre Pereira";
+
+      thunderbird = {
+        enable = true;
+        profiles = [ "pierre" ];
+      };
+
+      flavor = "gmail.com";
+      passwordCommand = private.pass-gmail;
+    };
+
+    "${private.polymtl}" = {
+      address = private.polymtl;
+      realName = "Pierre Pereira";
+      userName = private.user-polymtl;
+
+      thunderbird = {
+        enable = true;
+        profiles = [ "pierre" ];
+      };
+
+      passwordCommand = private.pass-polymtl;
+      smtp = {
+        host = "smtp.polymtl.ca";
+        port = 587;
+        tls = {
+          enable = true;
+          useStartTls = true;
+        };
+      };
+
+      imap = {
+        host = "imap.polymtl.ca";
+        port = 143;
+        tls.enable = false;
+      };
     };
   };
 }
