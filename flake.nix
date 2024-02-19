5533380d0bc0b19786bc8b036daf1ebaeb495580
Pierre Pereira <pierre.pereira@jolibrain.com>
2024-02-19 17:51:37 +0100

    📌 flake.lock, substituters

--- flake.nix
+++ flake.nix
@@ -1,6 +1,15 @@
 {
   description = "Pierrot's dotfiles";
 
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     home-manager = {
