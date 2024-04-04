1796bf34371c7f3c887479879ad7a40154a8e568
Pierrot LC <pierrotlc@proton.me>
2024-04-04 09:36:55 +0200

    feat(gnome): keybindings for workspaces

--- home/gnome.nix
+++ home/gnome.nix
@@ -22,7 +22,7 @@
       calculator = ["<Super>c"];
       control-center = ["<Super>s"];
       home = ["<Super>f"];
-      search = ["<Super>l"];
+      search = ["<Super>space"];
       www = ["<Super>b"];
       custom-keybindings = [
         "/org/gnome/setting-daemon/plugins/media-keys/custom-keybindings/custom0/"
@@ -36,6 +36,20 @@
     "org/gnome/shell/keybindings" = {
       toggle-message-tray = ["<Super>m"];
       toggle-quick-settings = ["disabled"];
+    };
+    "org/gnome/desktop/wm/keybindings" = {
+      move-to-monitor-left = ["<Shift><Super>h"];
+      move-to-monitor-right = ["<Shift><Super>l"];
+      move-to-workspace-1 = ["<Shift><Super>y"];
+      move-to-workspace-2 = ["<Shift><Super>u"];
+      move-to-workspace-3 = ["<Shift><Super>i"];
+      move-to-workspace-4 = ["<Shift><Super>o"];
+      switch-to-workspace-1 = ["<Super>y"];
+      switch-to-workspace-2 = ["<Super>u"];
+      switch-to-workspace-3 = ["<Super>i"];
+      switch-to-workspace-4 = ["<Super>o"];
+      switch-to-workspace-left = ["<Super>h"];
+      switch-to-workspace-right = ["<Super>l"];
     };
 
     # Extensions.
