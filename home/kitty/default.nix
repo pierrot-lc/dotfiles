c181d1323963f321fa9764418b3c8f824b5ebc0b
Pierrot LC <pierrotlc@proton.me>
2024-04-05 11:17:37 +0200

    feat(theme): kanagawa colorscheme

--- home/kitty/default.nix
+++ home/kitty/default.nix
@@ -1,16 +1,32 @@
-{theme, ...}: let
-  themeName = {
+{
+  lib,
+  theme,
+  ...
+}: let
+  # Those are the themes that are builtin in kitty. There's no need to source them.
+  builtinThemes = {
     "everforest" = "Everforest Dark Hard";
     "catppuccin" = "Catppuccin-Frappe";
   };
+  themeName =
+    if builtins.hasAttr theme builtinThemes
+    then builtinThemes.${theme}
+    else null;
+
+  # Those are the themes that are not builtin in kitty. They need to be sourced.
+  extraThemes = {
+    "kanagawa" = builtins.readFile ./kanagawa_dragon.conf;
+  };
+  extraConfig = lib.optionalString (builtins.hasAttr theme extraThemes) extraThemes.${theme};
 in {
   programs.kitty = {
     enable = true;
     extraConfig = builtins.concatStringsSep "\n" [
       (builtins.readFile ./kitty.conf)
       (builtins.readFile ./font.conf)
+      extraConfig
     ];
-    theme = themeName.${theme};
+    theme = themeName;
     shellIntegration.enableBashIntegration = true;
   };
 
