ce698be702f4d5d56112474734061b1c75aae262
Pierre Pereira <pierre.pereira@jolibrain.com>
2024-04-12 12:07:12 +0200

    feat(buku): git command aliases

--- home/buku.nix
+++ home/buku.nix
@@ -10,5 +10,7 @@
     b = "buku --suggest";
     bfzf = "buku -p -f 4 | fzf";
     burl = "buku -p -f 4 | fzf | cut --fields=2";
+    bpush = "cd ~/.local/share/buku/; git add --all; git commit --message='update db'; git push; cd -";
+    bpull = "cd ~/.local/share/buku/; git pull; cd -";
   };
 }
