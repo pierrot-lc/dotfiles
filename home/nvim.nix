046c1d170e76bc585b340c02bfea1118d9c7d780
Pierrot LC <pierrotlc@proton.me>
2024-04-16 08:42:03 +0200

    feat(nvim): installing some lsps and formatters here, others are to be installed per-project

--- home/nvim.nix
+++ home/nvim.nix
@@ -1,8 +1,23 @@
-{theme, ...}: {
+{pkgs, theme, ...}: {
   nvim-nix = {
     enable = true;
     inherit theme;
   };
+
+  # Add some LSPs and formatters. Others (such as pylsp or ruff-lsp)
+  # should be installed per-project in its flake declaration.
+  home.packages = with pkgs; [
+    alejandra
+    just
+    lua-language-server
+    marksman
+    nil
+    nodePackages_latest.bash-language-server
+    python311Packages.python-lsp-server
+    ruff-lsp
+    shfmt
+    stylua
+  ];
 
   home.sessionVariables = {
     EDITOR = "nvim";
