35e08b2f105f392d561d172dfd383e4aeebbb238
Pierre Pereira <pierrotlc@proton.me>
2024-04-22 11:27:47 +0200

    fix: string -> str type (string being deprecated)

--- modules/shell.nix
+++ modules/shell.nix
@@ -1,7 +1,7 @@
 {lib, ...}: {
   options = {
     shellAliases = lib.mkOption {
-      type = lib.types.attrsOf lib.types.string;
+      type = lib.types.attrsOf lib.types.str;
       default = {};
       description = "Shell aliases to be added to the user profile.";
     };
