2884322be6c682c69737464aebbceb3366db65a6
Pierre Pereira <pstmrtem@outlook.com>
2024-05-06 13:09:15 +0200

    feat(kitty): replaced local kanagawa theme with new builtin one

--- home/kitty/default.nix
+++ home/kitty/default.nix
@@ -1,35 +1,21 @@
-{
-  lib,
-  theme,
-  ...
-}: let
+{theme, ...}: let
   # Those are the themes that are builtin in kitty. There's no need to source them.
-  builtinThemes = {
+  themeParser = {
     "catppuccin" = "Catppuccin-Frappe";
     "everforest" = "Everforest Dark Hard";
     "gruvbox" = "Gruvbox Material Dark Soft";
     "rose-pine" = "Rosé Pine Moon";
     "rose-pine-dawn" = "Rosé Pine Dawn";
+    "kanagawa" = "Kanagawa_dragon";
   };
-  themeName =
-    if builtins.hasAttr theme builtinThemes
-    then builtinThemes.${theme}
-    else null;
-
-  # Those are the themes that are not builtin in kitty. They need to be sourced.
-  extraThemes = {
-    "kanagawa" = builtins.readFile ./kanagawa_dragon.conf;
-  };
-  extraConfig = lib.optionalString (builtins.hasAttr theme extraThemes) extraThemes.${theme};
 in {
   programs.kitty = {
     enable = true;
     extraConfig = builtins.concatStringsSep "\n" [
       (builtins.readFile ./kitty.conf)
       (builtins.readFile ./font.conf)
-      extraConfig
     ];
-    theme = themeName;
+    theme = themeParser.${theme};
     shellIntegration.enableBashIntegration = true;
   };
 
