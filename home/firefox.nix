cabbb884abcd7b1ffc9ddec793d80d653858a1f0
Pierre Pereira <pstmrtem@outlook.com>
2024-05-31 15:40:32 +0200

    redo: the correct link to the issue https://gitlab.gnome.org/GNOME/mutter/-/issues/3504

--- home/firefox.nix
+++ home/firefox.nix
@@ -8,6 +8,6 @@
 
   home.sessionVariables = {
     BROWSER = "firefox";
-    MOZ_ENABLE_WAYLAND = 0;  # Explicit-sync makes Firefox crash. See https://bugzilla.mozilla.org/show_bug.cgi?id=1898476.
+    MOZ_ENABLE_WAYLAND = 0;  # Explicit-sync makes Firefox crash. See https://gitlab.gnome.org/GNOME/mutter/-/issues/3504.
   };
 }
