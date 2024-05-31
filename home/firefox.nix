f74d085bfa361b67a55e0aae894c9f1378d14965
Pierre Pereira <pstmrtem@outlook.com>
2024-05-31 15:39:00 +0200

    fix: explicit sync with nvidia 555 makes firefox crash on wayland. See 'https://bugzilla.mozilla.org/show_bug.cgi?id=1898476'

--- home/firefox.nix
+++ home/firefox.nix
@@ -8,5 +8,6 @@
 
   home.sessionVariables = {
     BROWSER = "firefox";
+    MOZ_ENABLE_WAYLAND = 0;  # Explicit-sync makes Firefox crash. See https://bugzilla.mozilla.org/show_bug.cgi?id=1898476.
   };
 }
