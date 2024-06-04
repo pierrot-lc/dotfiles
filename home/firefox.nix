80c1bff43f35fa8a60d24f6cb3162638ef7bd1ef
Pierrot LC <pierrotlc@proton.me>
2024-06-04 08:28:39 +0200

    doc: firefox bug tracker

--- home/firefox.nix
+++ home/firefox.nix
@@ -8,6 +8,7 @@
 
   home.sessionVariables = {
     BROWSER = "firefox";
-    MOZ_ENABLE_WAYLAND = 0;  # Explicit-sync makes Firefox crash. See https://gitlab.gnome.org/GNOME/mutter/-/issues/3504.
+    # See https://bugzilla.mozilla.org/show_bug.cgi?id=1898476 and https://gitlab.gnome.org/GNOME/mutter/-/issues/3504.
+    MOZ_ENABLE_WAYLAND = 0;  # Explicit-sync on wayland makes Firefox crash.
   };
 }
