82ebb4fb591420f9d8fa48f5a7599ca655702d70
Pierrot LC <pierrotlc@proton.me>
2024-07-02 10:20:30 +0200

    feat(hosts): using latest nvidia for t15

--- hosts/t15/configuration.nix
+++ hosts/t15/configuration.nix
@@ -54,7 +54,7 @@
     nvidiaSettings = false;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
 
     # See
     # https://nixos.wiki/wiki/Nvidia#Laptop_Configuration:_Hybrid_Graphics_.28Nvidia_Optimus_PRIME.29.
