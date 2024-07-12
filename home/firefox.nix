b9934b96a3f5f4732bc90201c19fdac38a6269b1
Pierrot LC <pierrotlc@proton.me>
2024-07-13 00:05:17 +0200

    oopsie

--- home/firefox.nix
+++ home/firefox.nix
@@ -8,7 +8,7 @@
   # home.packages = [pkgs.firefoxpwa];
 
   home.sessionVariables = {
-    BROWSER = "firefox";
+    BROWSER = "librewolf";
     # See https://bugzilla.mozilla.org/show_bug.cgi?id=1898476 and https://gitlab.gnome.org/GNOME/mutter/-/issues/3504.
     MOZ_ENABLE_WAYLAND = 0; # Explicit-sync on wayland makes Firefox crash.
   };
