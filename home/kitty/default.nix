44a35f07c62d2b83463554427ebafed60c7c803e
Pierrot LC <pierrotlc@proton.me>
2024-09-22 16:26:14 +0200

    fix: updated kitty theme selection

--- home/kitty/default.nix
+++ home/kitty/default.nix
@@ -2,12 +2,12 @@
   # Those are the themes that are builtin in kitty. There's no need to source them.
   themeParser = {
     "catppuccin" = "Catppuccin-Frappe";
-    "everforest" = "Everforest Dark Hard";
-    "gruvbox" = "Gruvbox Material Dark Soft";
-    "kanagawa" = "Kanagawa_dragon";
+    "everforest" = "everforest_dark_hard";
+    "gruvbox" = "GruvboxMaterialDarkSoft";
+    "kanagawa" = "kanagawa_dragon";
     "nord" = "Nord";
-    "rose-pine" = "Rosé Pine Moon";
-    "rose-pine-dawn" = "Rosé Pine Dawn";
+    "rose-pine" = "rose-pine-moon";
+    "rose-pine-dawn" = "rose-pine-dawn";
   };
 in {
   programs.kitty = {
@@ -16,7 +16,7 @@ in {
       (builtins.readFile ./kitty.conf)
       (builtins.readFile ./font.conf)
     ];
-    theme = themeParser.${config.theme.name};
+    themeFile = themeParser.${config.theme.name};
     shellIntegration.enableBashIntegration = true;
   };
 
