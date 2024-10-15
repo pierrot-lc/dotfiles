aa6ad62b55af19a1301bec0cd698bc87b1dd69d1
Pierrot LC <pierrotlc@proton.me>
2024-10-15 09:40:35 +0200

    fix: typo

--- home/myrepos.nix
+++ home/myrepos.nix
@@ -3,7 +3,7 @@
     enable = true;
     settings = {
       "${config.xdg.dataHome}/password-store" = {
-        checkout = "git clone git@gitea.com:pierrot-lc/buku.git";
+        checkout = "git clone git@gitea.com:pierrot-lc/password-store.git";
       };
       "${config.xdg.dataHome}/buku" = {
         checkout = "git clone git@gitea.com:pierrot-lc/buku.git";
