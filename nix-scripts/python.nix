5a302ecc10147f19d67ddc289aeb4d21d2a5bcca
Pierrot LC <pierrotlc@proton.me>
2024-10-29 22:42:12 +0100

    nix-scripts: python links

--- nix-scripts/python.nix
+++ nix-scripts/python.nix
@@ -1,3 +1,6 @@
+# Useful links:
+# - [Nixos Python Wiki](https://wiki.nixos.org/wiki/Python)
+# - [Github Python Wiki](https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/python.section.md)
 {pkgs ? import <nixpkgs>}: let
   python-packages = ps:
     with ps; [
