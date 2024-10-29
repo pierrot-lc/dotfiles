98754d60a56ff0bfb3754ccc6fdcfc6be9d721fc
Pierrot LC <pierrotlc@proton.me>
2024-10-29 23:30:21 +0100

    feat(librewolf): bukubrow extension

--- home/firefox.nix
+++ home/firefox.nix
@@ -1,6 +1,13 @@
 {pkgs, ...}: {
   librewolf-nix = {
     enable = true;
+    extraExtensions = {
+      # Bukubrow
+      "bukubrow@samhh.com" = {
+        install_url = "https://addons.mozilla.org/firefox/downloads/file/3769984/bukubrow-latest.xpi";
+        installation_mode = "force_installed";
+      };
+    };
     nativeMessagingHosts = with pkgs; [bukubrow];
   };
 
