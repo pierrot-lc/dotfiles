657b773f3a8220e86716a96739117a1bb64d602e
Pierrot LC <pierrotlc@proton.me>
2024-11-01 19:03:31 +0100

    feat(buku): adding the buku merger :D!

--- home/buku.nix
+++ home/buku.nix
@@ -6,9 +6,15 @@
 
   shellAliases = {
     b = "buku --suggest";
-    buku-pull = "cd $XDG_DATA_HOME/buku/; git pull; cd -";
-    buku-push = "cd $XDG_DATA_HOME/buku/; git add --all; git commit --message='update db'; git push; cd -";
     fzf-buku = "buku -p -f 4 | fzf";
     fzf-buku-url = "buku -p -f 4 | fzf | cut --fields=2";
+  };
+
+  programs.git.extraConfig = {
+    merge.buku-driver = {
+      name = "Custom buku merge driver, handling the bookmarks SQLite database";
+      driver = "nix run 'github:pierrot-lc/buku-merger' -- %O %A %B";
+      recursive = "binary";
+    };
   };
 }
