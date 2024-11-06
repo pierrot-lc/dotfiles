a1c2bb1864c03a32f4e150b858dca3bac191e2c6
Pierrot LC <pierrotlc@proton.me>
2024-11-06 10:55:06 +0100

    fix: python nix shell

--- nix-scripts/python.nix
+++ nix-scripts/python.nix
@@ -1,7 +1,7 @@
 # Useful links:
 # - [Nixos Python Wiki](https://wiki.nixos.org/wiki/Python)
 # - [Github Python Wiki](https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/python.section.md)
-{pkgs ? import <nixpkgs>}: let
+{pkgs ? import <nixpkgs> {}}: let
   python-packages = ps:
     with ps; [
       pip
