1dd6086bc08d3ed3fd8392e57240d3f585a6a1df
Pierrot LC <pierrotlc@proton.me>
2024-12-03 01:32:46 +0100

    feat(starship): prompt conf

--- home/programs.nix
+++ home/programs.nix
@@ -32,6 +32,7 @@
     enableBashIntegration = true;
     settings = {
       add_newline = false;
+      directory.truncate_to_repo = false;
       time.disabled = false;
       nix_shell = {
         format = "via [$symbol(\($name\))]($style) ";
