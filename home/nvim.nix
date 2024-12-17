a47e1bc335b8bc78d2476b63c055ca418524e8aa
Pierrot LC <pierrotlc@proton.me>
2024-12-17 08:30:28 +0100

    feat(nvim): switch from pylsp to pyright

--- home/nvim.nix
+++ home/nvim.nix
@@ -12,13 +12,13 @@
   # Add some LSPs and formatters.
   home.packages = with pkgs; [
     alejandra
+    basedpyright
     bash-language-server
     inotify-tools # https://github.com/neovim/neovim/pull/29374.
     just
     lua-language-server
     marksman
     nixd
-    python311Packages.python-lsp-server
     ruff
     shfmt
     stylua
