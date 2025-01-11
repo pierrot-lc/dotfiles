55172e2e4ecc83d6ec6b79eaf7590441b281242b
Pierrot LC <pierrotlc@proton.me>
2025-01-11 19:21:00 +0100

    feat(television): bat-theme for preview and ui

--- home/television.nix
+++ home/television.nix
@@ -7,6 +7,10 @@
   home.file."${config.xdg.configHome}/television/config.toml".text = /* toml */ ''
       [ui]
       use_nerd_font_icons = true
+      input_bar_position = "top"
+
+      [previewers.file]
+      theme = "${config.programs.bat.config.theme}"
     '';
 
   home.shellAliases = {
