7a8c300f67d10a1441bf4cd5c1b63648cdd9d457
Pierrot LC <pierrotlc@proton.me>
2025-03-11 02:42:12 +0100

    password hash

--- hosts/raspi-4/configuration.nix
+++ hosts/raspi-4/configuration.nix
@@ -9,6 +9,9 @@
 
   boot.kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
 
+  # User password hash.
+  users.users.pierrot-lc.hashedPasswordFile = "/run/secrets/passwd-hash";
+
   # Overwrite some default settings.
   services.printing.enable = false;
   services.avahi.enable = false;
