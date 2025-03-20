887a5ec32aab072595d56050a3f14edf97c7aae1
Pierrot LC <pierrotlc@proton.me>
2025-03-20 10:02:35 +0100

    feat(librewolf): bye bye bukubrow :(

--- home/gui-apps/librewolf.nix
+++ home/gui-apps/librewolf.nix
@@ -1,12 +1,7 @@
-{pkgs, ...}: {
+{
   librewolf-nix = {
     enable = true;
     extraExtensions = {
-      # Bukubrow
-      "bukubrow@samhh.com" = {
-        install_url = "https://addons.mozilla.org/firefox/downloads/file/3769984/bukubrow-latest.xpi";
-        installation_mode = "force_installed";
-      };
       # Proton Pass
       "78272b6fa58f4a1abaac99321d503a20@proton.me" = {
         install_url = "https://addons.mozilla.org/firefox/downloads/file/4440138/proton_pass-latest.xpi";
@@ -18,7 +13,6 @@
         installation_mode = "force_installed";
       };
     };
-    nativeMessagingHosts = with pkgs; [bukubrow];
   };
 
   home.sessionVariables = {
