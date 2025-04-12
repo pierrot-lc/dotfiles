bdcf19eed38461562567d04be94293485c8f2548
Pierrot LC <pierrotlc@proton.me>
2025-04-12 12:26:51 +0200

    print

--- configuration/printing.nix
+++ configuration/printing.nix
@@ -1,14 +1,24 @@
-{pkgs, lib, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   # Enable CUPS to print documents.
   services.printing = {
     enable = lib.mkDefault true;
     drivers = [pkgs.cnijfilter2];
   };
 
+  users.extraGroups.lpadmin.members = ["pierrot-lc"];
+
   # Automatically detect new printers over the network.
   services.avahi = {
     enable = lib.mkDefault true;
     nssmdns4 = true;
     openFirewall = true;
+    publish = {
+      enable = true;
+      userServices = true;
+    };
   };
 }
