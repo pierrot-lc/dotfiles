fdebd5235bb4d4da130aaec376f85ad9bb405ac0
Pierre Pereira <pierrot-lc@proton.me>
2025-04-23 11:47:25 +0200

    configuration: removed via software

--- configuration/keyboards.nix
+++ configuration/keyboards.nix
@@ -6,7 +6,4 @@
   # Enable special udev rules for my keyboards.
   hardware.keyboard.qmk.enable = true;
   hardware.keyboard.zsa.enable = true;
-
-  environment.systemPackages = [pkgs.via];
-  services.udev.packages = [pkgs.via];
 }
