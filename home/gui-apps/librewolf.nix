7e70592e25aa2f5a313b520f185012b6c14d5182
Pierrot LC <pierrotlc@proton.me>
2025-07-25 16:07:02 +0200

    feat(librewolf): extension default area

--- home/gui-apps/librewolf.nix
+++ home/gui-apps/librewolf.nix
@@ -6,11 +6,13 @@
       "78272b6fa58f4a1abaac99321d503a20@proton.me" = {
         install_url = "https://addons.mozilla.org/firefox/downloads/file/4440138/proton_pass-latest.xpi";
         installation_mode = "force_installed";
+        default_area = "navbar";
       };
       # Proton VPN
       "vpn@proton.ch" = {
         install_url = "https://addons.mozilla.org/firefox/downloads/file/4413146/proton_vpn_firefox_extension-latest.xpi";
         installation_mode = "force_installed";
+        default_area = "navbar";
       };
     };
   };
