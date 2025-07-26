b3de66296ff0aa17b7950766f11e4db40b3fa551
Pierrot LC <pierrotlc@proton.me>
2025-07-27 00:43:44 +0200

    fix(librewolf): proton-pass url

--- home/gui-apps/librewolf.nix
+++ home/gui-apps/librewolf.nix
@@ -4,7 +4,7 @@
     extraExtensions = {
       # Proton Pass
       "78272b6fa58f4a1abaac99321d503a20@proton.me" = {
-        install_url = "https://addons.mozilla.org/firefox/downloads/file/4440138/proton_pass-latest.xpi";
+        install_url = "https://addons.mozilla.org/firefox/downloads/file/4524394/proton_pass-latest.xpi";
         installation_mode = "force_installed";
         default_area = "navbar";
       };
