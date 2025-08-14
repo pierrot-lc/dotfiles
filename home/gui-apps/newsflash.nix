2b2f518b76fff7e931a6f707ad94a3293b071765
Pierrot LC <pierrotlc@proton.me>
2025-08-14 20:18:38 +0200

    newsflash: temporarily disable my account

--- home/gui-apps/newsflash.nix
+++ home/gui-apps/newsflash.nix
@@ -11,7 +11,7 @@ in {
   home.packages = [pkgs.newsflash];
 
   # Add the freshrss account.
-  home.file."${freshrssJsonPath}".text = builtins.toJSON private.newsflash;
+  # home.file."${freshrssJsonPath}".text = builtins.toJSON private.newsflash;
 
   # Specify the backend to be freshrss. The config file should remain editable
   # so the file is generated from the following activation script.
@@ -20,10 +20,10 @@ in {
       # Create the JSON file if it does not exists already. The JSON file is
       # automatically created when the application is launched. We use a dumb
       # command to do so.
-      ${pkgs.newsflash}/bin/io.gitlab.news_flash.NewsFlash -h > /dev/null
+      # ${pkgs.newsflash}/bin/io.gitlab.news_flash.NewsFlash -h > /dev/null
 
       # Add the freshrss backend.
-      cat "${newsflashJsonPath}" | ${pkgs.jq}/bin/jq --arg "backend" "freshrss" '. + $ARGS.named' > "${newsflashJsonPath}.tmp"
-      mv "${newsflashJsonPath}.tmp" "${newsflashJsonPath}"
+      # cat "${newsflashJsonPath}" | ${pkgs.jq}/bin/jq --arg "backend" "freshrss" '. + $ARGS.named' > "${newsflashJsonPath}.tmp"
+      # mv "${newsflashJsonPath}.tmp" "${newsflashJsonPath}"
     '';
 }
