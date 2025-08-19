f51495c83c7c9eb005cfa8c09f711c0ebfca294a
Pierrot LC <pierrotlc@proton.me>
2025-08-20 01:25:44 +0200

    librewolf: fix proton-vpn extension url?

--- home/gui-apps/librewolf.nix
+++ home/gui-apps/librewolf.nix
@@ -10,7 +10,7 @@
       };
       # Proton VPN
       "vpn@proton.ch" = {
-        install_url = "https://addons.mozilla.org/firefox/downloads/file/4413146/proton_vpn_firefox_extension-latest.xpi";
+        install_url = "https://addons.mozilla.org/firefox/downloads/file/4539502/proton_vpn_firefox_extension-1.2.9.xpi";
         installation_mode = "force_installed";
         default_area = "navbar";
       };
