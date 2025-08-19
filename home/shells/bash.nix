2bdfab45b7e7b61e3aba8dcc683cea8447bd5f23
Pierrot LC <pierrotlc@proton.me>
2025-08-19 22:20:43 +0200

    shell: go back to bash for a while

--- home/shells/bash.nix
+++ home/shells/bash.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{
   programs.bash = {
     enable = true;
     initExtra = /* bash */ ''
@@ -6,14 +6,6 @@
       if [ "$TERM" = "xterm-kitty" ]; then
           alias icat="kitty +kitten icat"
           alias ssh="kitty +kitten ssh"
-      fi
-
-      # Launches fish when entering an interactive shell.
-      # See https://nixos.wiki/wiki/Fish#Setting_fish_as_your_shell
-      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-      then
-        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
       fi
     '';
     historyIgnore = [
--- home/shells/fish.nix
+++ home/shells/fish.nix
@@ -1,6 +1,6 @@
-{
+{pkgs, ...}: {
   programs.fish = {
-    enable = true;
+    enable = false;
     interactiveShellInit = /* fish */ ''
         if test "$TERMINAL" = "kitty"
           alias icat="kitty +kitten icat"
@@ -8,4 +8,14 @@
         end
       '';
   };
+
+  programs.bash.initExtra = /* bash */ ''
+      # Launches fish when entering an interactive shell.
+      # See https://nixos.wiki/wiki/Fish#Setting_fish_as_your_shell
+      # if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+      # then
+      #   shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+      #   exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+      # fi
+  '';
 }
