57dc59fdc538aba94cef0600e7a4f78ccc9aaf1c
Pierre Pereira <pierre.pereira@proton.me>
2025-09-28 01:24:51 +0200

    feat(home): xdg defaults

--- home/gui-apps/librewolf.nix
+++ home/gui-apps/librewolf.nix
@@ -17,14 +17,6 @@
     };
   };
 
-  xdg.mimeApps.defaultApplications = {
-    "text/html" = "librewolf.desktop";
-    "x-scheme-handler/http" = "librewolf.desktop";
-    "x-scheme-handler/https" = "librewolf.desktop";
-    "x-scheme-handler/about" = "librewolf.desktop";
-    "x-scheme-handler/unknown" = "librewolf.desktop";
-  };
-
   home.sessionVariables = {
     BROWSER = "librewolf";
     MOZ_ENABLE_WAYLAND = 1;
