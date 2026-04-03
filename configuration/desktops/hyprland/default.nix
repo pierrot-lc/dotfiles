596302dec6bc5d5a789a167dc6d3738e1d457d06
Pierrot LC <pierrotlc@proton.me>
2026-04-03 11:10:00 +0200

    feat(hyprland): embed hyprland.conf within hyprland declaration

--- configuration/desktops/hyprland/default.nix
+++ configuration/desktops/hyprland/default.nix
@@ -8,14 +8,14 @@
     # Enable Hyprland.
     programs.hyprland.enable = true;
     programs.hyprland.withUWSM = true;
-    # programs.hyprland.package = pkgs.hyprland.overrideAttrs (oldAttrs: {
-    #   buildInputs = oldAttrs.buildInputs or [] ++ [pkgs.makeWrapper];
-    #   postInstall =
-    #     oldAttrs.postInstall or ""
-    #     + ''
-    #       wrapProgram $out/bin/Hyprland --append-flags "--config ${./hyprland.conf}"
-    #     '';
-    # });
+    programs.hyprland.package = pkgs.hyprland.overrideAttrs (oldAttrs: {
+      buildInputs = oldAttrs.buildInputs or [] ++ [pkgs.makeWrapper];
+      postInstall =
+        oldAttrs.postInstall or ""
+        + ''
+          wrapProgram $out/bin/Hyprland --append-flags "--config ${./hypr/hyprland.conf}"
+        '';
+    });
 
     services.displayManager.gdm.enable = true;
     services.displayManager.autoLogin.enable = true;
@@ -24,6 +24,7 @@
     services.upower.enable = true;
 
     environment.systemPackages = with pkgs; [
+      ashell
       brightnessctl
       gnome-text-editor
       hyprlauncher
